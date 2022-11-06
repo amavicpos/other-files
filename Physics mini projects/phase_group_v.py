0db2efeda156783795585c1ad48589d609a010e9
amavicpos <100805638+amavicpos@users.noreply.github.com>
2022-11-06 00:01:44 +0000

    Create wave equation function

--- Physics mini projects/phase_group_v.py
+++ Physics mini projects/phase_group_v.py
@@ -3,20 +3,28 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 fig, ax = plt.subplots()
-xdata, ydata = [], []
+tdata, ydata = [], []
 ln, = ax.plot([], [], 'ro')
 
+w = 5
+
+def wave_equation(x, t, A=1, k=2*np.pi, w=2*np.pi, phi=0, dir=-1):
+    return A*np.cos(k*x + dir*w*t + phi)
+
 def init():
-    ax.set_xlim(0, 2*np.pi/5)
+    ax.set_xlim(0, 2*np.pi/w)
     ax.set_ylim(-1, 1)
     return ln,
 
 def update(frame):
-    xdata.append(frame)
-    ydata.append(np.sin(5*frame))
-    ln.set_data(xdata, ydata)
+    tdata.append(frame)
+    ydata.append(wave_equation(0, frame, w=w))
+    ln.set_data(tdata, ydata)
     return ln,
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi/5, 10),
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi/w, 20),
                     init_func=init, blit=True)
 plt.show()
+
+# TODO: Subplots: function of t and function of x
+# TODO: Format plots
