4d827d2b87687244f1447c64a35f117ae6e53235
amavicpos <100805638+amavicpos@users.noreply.github.com>
2022-11-06 07:29:28 +0000

    Change animation functions

--- Physics mini projects/phase_group_v.py
+++ Physics mini projects/phase_group_v.py
@@ -1,11 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 
-fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
-x1data, y1data, x2data, y2data = [], [], [], []
-ln1, = ax1.plot([], [], 'ro')
-ln2, = ax2.plot([], [], 'ko')
+# TODO: Format plots
+# TODO: Specify set t or x in plot automatically
 
 w = 5
 k = 2*np.pi
@@ -13,40 +11,60 @@ k = 2*np.pi
 def wave_equation(x, t, A=1, k=2*np.pi, w=2*np.pi, phi=0, dir=-1):
     return A*np.cos(k*x + dir*w*t + phi)
 
-def init1():
-    ax1.set_xlim(0, 2*np.pi/w)
-    ax1.set_ylim(-1, 1)
-    return ln1,
+def data_gen():
+    t = data_gen.t
+    x = data_gen.x
+    cnt = 0
+    while cnt < 1e6:
+        cnt+=1
+        t += 2*np.pi/w/100
+        x += 2*np.pi/k/100
+        y1 = wave_equation(0, t)
+        y2 = wave_equation(x, 0)
+        # adapted the data generator to yield both sin and cos
+        yield t, x, y1, y2
 
-def update1(frame):
-    x1data.append(frame)
-    y1data.append(wave_equation(0, frame, w=w))
-    ln1.set_data(x1data, y1data)
-    return ln1,
+data_gen.t = 0
+data_gen.x = 0
 
-def init2():
-    ax2.set_xlim(0, 2*np.pi/k)
-    ax2.set_ylim(-1, 1)
-    return ln2,
+# create a figure with two subplots
+fig, (ax1, ax2) = plt.subplots(2,1)
 
-def update2(frame):
-    x2data.append(frame)
-    y2data.append(wave_equation(frame, 0, k=k))
-    ln2.set_data(x2data, y2data)
-    return ln2,
+# intialize two line objects (one in each axes)
+line1, = ax1.plot([], [], lw=2)
+line2, = ax2.plot([], [], lw=2, color='r')
+line = [line1, line2]
 
-ani1 = FuncAnimation(fig, update1, frames=np.linspace(0, 2*np.pi/w, 20),
-                    init_func=init1, blit=True)
-ani2 = FuncAnimation(fig, update2, frames=np.linspace(0, 2*np.pi/k, 20),
-                    init_func=init2, blit=True)
+ax1.set_ylim(-1, 1)
+ax1.set_xlim(0, 2*np.pi/w)
+ax1.grid()
+ax2.set_ylim(-1, 1)
+ax2.set_xlim(0, 2*np.pi/k)
+ax2.grid()
 
-ax1.set_xlabel('Time (s)')
-ax1.set_ylabel('Amplitude (m)')
+# initialize the data arrays 
+tdata, xdata, y1data, y2data = [], [], [], []
+def run(data):
+    # update the data
+    t, x, y1, y2 = data
+    tdata.append(t)
+    xdata.append(x)
+    y1data.append(y1)
+    y2data.append(y2)
 
-ax2.set_xlabel('Position x (m)')
-ax2.set_ylabel('Amplitude (m)')
+    # axis limits checking. Same as before, just for both axes
+    for ax in [ax1, ax2]:
+        xmin, xmax = ax.get_xlim()
+        if t >= xmax:
+            ax.set_xlim(xmin, 2*xmax)
+            ax.figure.canvas.draw()
 
+    # update the data of both line objects
+    line[0].set_data(tdata, y1data)
+    line[1].set_data(xdata, y2data)
+
+    return line
+
+ani = animation.FuncAnimation(fig, run, data_gen, blit=True, interval=10,
+    repeat=False)
 plt.show()
-
-# TODO: Format plots
-# TODO: Specify set t or x in plot automatically
