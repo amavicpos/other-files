3f9dc9abc3a1efa71ddc93ed43d6837b15d2ff21
amavicpos <100805638+amavicpos@users.noreply.github.com>
2022-11-08 09:09:08 +0000

    Change function name

--- Physics mini projects/phase_group_v.py
+++ Physics mini projects/phase_group_v.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # TODO: Plot more than one curve in same graph
-# TODO: Plot oscillation inside beat and envelope separately
+# TODO: Plot oscillation inside and envelope separately
 # TODO: Format plot
 
 T1, T2 = 1, 1
@@ -14,10 +14,10 @@ x_range = np.linspace(0, l1*100, 100)
 t_range = np.linspace(0, T1*100, 100)
 A=1
 
-def beats(x, t, A=1, k1=2*np.pi, k2=2*np.pi, w1=2*np.pi, w2=2*np.pi):
+def sum_wave(x, t, A=1, k1=2*np.pi, k2=2*np.pi, w1=2*np.pi, w2=2*np.pi):
     return 2*A*np.cos((k1-k2)/2*x-(w1-w2)/2*t)*np.cos((k1+k2)/2*x-(w1+w2)/2*t)
 
-matrix_ = np.matrix([[beats(x, t, A, k1, k2, w1, w2) for x in x_range] for t in t_range])
+matrix_ = np.matrix([[sum_wave(x, t, A, k1, k2, w1, w2) for x in x_range] for t in t_range])
 
 fig, ax = plt.subplots()
 xdata, ydata = [], []
