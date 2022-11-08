54946df0f5bc38d07f181cefdce64812dcff09ea
amavicpos <100805638+amavicpos@users.noreply.github.com>
2022-11-08 17:21:38 +0000

    Create basic complete plot

--- Physics mini projects/phase_group_v.py
+++ Physics mini projects/phase_group_v.py
@@ -2,36 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# TODO: Plot more than one curve in same graph
-# TODO: Plot oscillation inside and envelope separately
+# TODO: Change values so that plot looks nice
 # TODO: Format plot
 
-T1, T2 = 1, 1
-l1, l2 = 1, 1
-w1, w2 = 2*np.pi/T1, 2*np.pi/T2
-k1, k2 = 2*np.pi/l1, 2*np.pi/l2
-x_range = np.linspace(0, l1*100, 100)
-t_range = np.linspace(0, T1*100, 100)
-A=1
+A = 1
+N = 100
+w1, w2 = 1, 1.001
+k1, k2 = 1, 1.001
+T1, T2 = 2*np.pi/w1, 2*np.pi/w2
+l1, l2 = 2*np.pi/k1, 2*np.pi/k2
+x_range = np.linspace(0, l1*1000, N)
+t_range = np.linspace(0, T1*1000, N)
+
+def envelope(x, t, A=1, k1=2*np.pi, k2=2*np.pi, w1=2*np.pi, w2=2*np.pi):
+    return 2*A*np.cos((k1-k2)/2*x+(w1-w2)/2*t)
 
 def sum_wave(x, t, A=1, k1=2*np.pi, k2=2*np.pi, w1=2*np.pi, w2=2*np.pi):
-    return 2*A*np.cos((k1-k2)/2*x-(w1-w2)/2*t)*np.cos((k1+k2)/2*x-(w1+w2)/2*t)
+    return envelope(x, t, A, k1, k2, w1, w2)*np.cos((k1+k2)/2*x-(w1+w2)/2*t)
 
-matrix_ = np.matrix([[sum_wave(x, t, A, k1, k2, w1, w2) for x in x_range] for t in t_range])
+envelope_ = np.matrix([[envelope(x, t, A, k1, k2, w1, w2) for x in x_range] for t in t_range])
+sum_wave_ = np.matrix([[sum_wave(x, t, A, k1, k2, w1, w2) for x in x_range] for t in t_range])
 
 fig, ax = plt.subplots()
-xdata, ydata = [], []
-ln, = ax.plot([], [], 'r')
+ln1, = ax.plot([], [], 'b')
+ln2, = ax.plot([], [], 'r')
 
 def init():
-    ax.set_xlim(0, l1*100)
-    ax.set_ylim(-2*A, 2*A)
-    return ln,
+    ax.set_xlim(0, l1*1000)
+    ax.set_ylim(-3*A, 3*A)
+    return ln1, ln2,
 
 def update(frame):
-    ln.set_data(x_range, matrix_[frame])
-    return ln,
+    ln1.set_data(x_range, envelope_[frame])
+    ln2.set_data(x_range, sum_wave_[frame])
+    return ln1, ln2,
 
-ani = FuncAnimation(fig, update, frames=range(len(matrix_)),
+ani = FuncAnimation(fig, update, frames=range(len(sum_wave_)),
                     init_func=init, blit=True)
 plt.show()
