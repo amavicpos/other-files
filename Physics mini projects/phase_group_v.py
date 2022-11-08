c359e2180054ec37d4a8b6745a7bc09140e8d5f5
amavicpos <100805638+amavicpos@users.noreply.github.com>
2022-11-08 18:25:51 +0000

    Put list comprehensions back and change values

--- Physics mini projects/phase_group_v.py
+++ Physics mini projects/phase_group_v.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# TODO: Improve shape of function, ensure it's correct
+# TODO: Change values, plot different cases
 # TODO: Format plot
 
 A = 1
-N = 100
-w1, w2 = 1, 1.001
-k1, k2 = 1, 1.001
-T1, T2 = 2*np.pi/w1, 2*np.pi/w2
-l1, l2 = 2*np.pi/k1, 2*np.pi/k2
-x_range = np.linspace(0, l1*1000, N)
-t_range = np.linspace(0, T1*1000, N)
+N = 500
+w1, w2 = 1.001, 1
+k1, k2 = 1.001, 1
+l_env = 2*np.pi/((k1-k2)/2)
+T_env = 2*np.pi/((w1-w2)/2)
+x_range = np.linspace(0, l_env*2, N)
+t_range = np.linspace(0, T_env*5, N)
 
 def envelope(x, t, A=1, k1=2*np.pi, k2=2*np.pi, w1=2*np.pi, w2=2*np.pi):
     return 2*A*np.cos((k1-k2)/2*x+(w1-w2)/2*t)
@@ -20,19 +20,15 @@ def envelope(x, t, A=1, k1=2*np.pi, k2=2*np.pi, w1=2*np.pi, w2=2*np.pi):
 def sum_wave(x, t, A=1, k1=2*np.pi, k2=2*np.pi, w1=2*np.pi, w2=2*np.pi):
     return envelope(x, t, A, k1, k2, w1, w2)*np.cos((k1+k2)/2*x-(w1+w2)/2*t)
 
-envelope_ = np.zeros((N,)*2)
-sum_wave_ = np.zeros((N,)*2)
-for i, t in enumerate(t_range):
-    for j, x in enumerate(x_range):
-        envelope_[i][j] = envelope(x, t, A, k1, k2, w1, w2)
-        sum_wave_[i][j] = sum_wave(x, t, A, k1, k2, w1, w2)
+envelope_ = [[envelope(x, t, A, k1, k2, w1, w2) for x in x_range] for t in t_range]
+sum_wave_ = [[sum_wave(x, t, A, k1, k2, w1, w2) for x in x_range] for t in t_range]
 
 fig, ax = plt.subplots()
 ln1, = ax.plot([], [], 'b')
 ln2, = ax.plot([], [], 'r')
 
 def init():
-    ax.set_xlim(0, l1*1000)
+    ax.set_xlim(0, l_env*2)
     ax.set_ylim(-3*A, 3*A)
     return ln1, ln2,
 
