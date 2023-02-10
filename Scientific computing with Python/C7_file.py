a1f8b5a3233bd82220aec1f27665953350faceee
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-02-10 13:18:03 +0000

    Upload C7 files

---
+++ Scientific computing with Python/C7_file.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import random as rd
+
+# READING AND WRITING TO A FILE
+rho, rad = np.loadtxt("data/data_1.dat", skiprows=1, unpack=True)
+t, h, u = np.loadtxt("data/data_2.dat", skiprows=2,
+                     delimiter=",", unpack=True)
+out_array = np.array([t, h, u])
+# np.savetxt("data/test_out.dat", out_array.T, fmt="%8.1f")
+
+
+# GRAPH PLOTTING
+plt.plot(t, h)
+plt.show()
+
+t = np.linspace(-10.0, 10.0, 200)            # create t array of theory data
+v = np.sin(t) * np.exp(-(t/5.0)**2)  # "    v array      "    "
+
+plt.figure(1, figsize=(8, 6))              # create plot, specifying size
+plt.plot(t, v, "b-", label="theory")         # "b-", colour blue, solid line
+
+plt.xlabel('Time (ms)')
+plt.ylabel('Transverse Displacement (volts)')
+plt.legend(loc="upper right")
+
+plt.axhline(color="gray", zorder=-1)         # horizontal line
+plt.axvline(color="gray", zorder=-1)         # vertical line
+
+# saves file; could be .pdf, .jpg
+plt.savefig("data/wavy_pulse.png")
+
+plt.show()                                   # display plot on screen
+
+t_data, v_data = np.loadtxt("data/pulse_data.dat", skiprows=2, unpack=True)
+plt.plot(t_data, v_data, "ro")
+plt.errorbar(t_data, v_data, fmt="ro", label="expt", xerr=0.5, ecolor="black")
+plt.show()
+
+x_data = np.random.uniform(0.0, 100.0, 20)
+y_data = 3.0 * x_data + 35.0 + np.random.uniform(-15.0, 15.0, 20)
+eb = np.repeat(15.0, 20)
+plt.errorbar(x_data, y_data, yerr=eb, fmt="x")
+
+
+# LINEAR REGRESSION
+
+def str_line(x, m, c):
+    return m * x + c
+
+
+popt, pcov = curve_fit(str_line, x_data, y_data, sigma=None)
+print("Linear Regression:")
+print("a =", popt[0], "+/-", pcov[0, 0]**0.5)
+print("b =", popt[1], "+/-", pcov[1, 1]**0.5)
+x_line = np.linspace(0.0, 100.0, 100)  # define a set of x-values
+plt.plot(x_line, str_line(x_line, *popt), 'r-')  # y-values using str_line()
+plt.show()
+
+
+# EXERCISES
+# Monte Carlo
+N, radius = 200, 1
+x_points, y_points = [
+    rd.uniform(-1, 1) for i in range(N)], [rd.uniform(-1, 1) for i in range(N)]
+
+
+def inside_circle(x, y): return (x ** 2 + y ** 2) <= radius
+
+
+circ = sum(1 for x, y in zip(x_points, y_points) if inside_circle(x, y))
+print("\nMonte Carlo:")
+print(f"Percentage of points inside circle: {circ/N*100}%")
+print(f"Theoretical percentage: {round(np.pi/4*100, 1)}%\n")
+x_circle = [np.cos(v) for v in np.arange(0, np.pi * 2, 0.01)]
+y_circle = [np.sin(v) for v in np.arange(0, np.pi * 2, 0.01)]
+plt.scatter(x_points, y_points)
+plt.plot(x_circle, y_circle, 'r')
+plt.axis('square')
+plt.show()
+
+# Stefan's Law
+T, P = np.loadtxt("data/stefan.dat", skiprows=2, unpack=True)
+logT, logP = np.log(T), np.log(P)
+plt.plot(logT, logP)
+plt.xlabel('log(T)')
+plt.ylabel('log(P)')
+plt.show()
+popt, pcov = curve_fit(str_line, logT, logP, sigma=None)
+print("Stefan's Law, linear regression, a is the calculated exponent of T:")
+print("a =", popt[0], "+/-", pcov[0, 0]**0.5)
+print("b =", popt[1], "+/-", pcov[1, 1]**0.5)
