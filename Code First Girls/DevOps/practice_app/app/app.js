b9532d09e2aafdf1d44845c93a52946baf60e38c
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-07-09 22:59:42 +0100

    Use ejs in practice app

--- Code First Girls/DevOps/practice_app/app/app.js
+++ Code First Girls/DevOps/practice_app/app/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = 3000;
 
-app.set('view engine', 'ejs');
+// Set the view engine to EJS
 app.set('views', path.join(__dirname, 'views'));
 
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 
+// Define a route to render the EJS template
 app.get('/', (req, res) => {
-    res.render('home')
+  res.render('home', { message: 'Hello EJS!' });
 });
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
