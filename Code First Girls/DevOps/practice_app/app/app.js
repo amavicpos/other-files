a76a18464128aa27c62610d19ea85d091af0a8a2
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-07-10 07:31:16 +0100

    Add utils functions

--- Code First Girls/DevOps/practice_app/app/app.js
+++ Code First Girls/DevOps/practice_app/app/app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+
+const {add} = require('./utils/math');
+
 const app = express();
-const port = 3000;
 
-// Set the view engine to EJS
 app.set('views', path.join(__dirname, 'views'));
-
-// Set the view engine to EJS
 app.set('view engine', 'ejs');
 
-// Define a route to render the EJS template
 app.get('/', (req, res) => {
-  res.render('home', { message: 'Hello EJS!' });
+    const data = {
+        name: 'John',
+        age: add(17, 1)
+    };
+
+    res.render('home', { data });
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
 });
